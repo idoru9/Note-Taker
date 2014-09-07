ec1890a52a0fa5bcb6deab08227e943ad2024b8a
richard@richard-fellows.com <richard@richard-fellows.com>
2014-09-07 15:22:24 +0100

    initial commit

---
+++ Note Taker/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿using System.Windows.Forms;
+namespace Note_Taker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input_textbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // input_textbox
+            // 
+            this.input_textbox.AcceptsReturn = true;
+            this.input_textbox.BackColor = System.Drawing.SystemColors.Control;
+            this.input_textbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.input_textbox.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input_textbox.Location = new System.Drawing.Point(12, 12);
+            this.input_textbox.MaximumSize = new System.Drawing.Size(500, 15);
+            this.input_textbox.Name = "input_textbox";
+            this.input_textbox.Size = new System.Drawing.Size(500, 15);
+            this.input_textbox.TabIndex = 0;
+            this.input_textbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.input_textbox_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(532, 39);
+            this.Controls.Add(this.input_textbox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input_textbox;
+    }
+}
+
---
+++ Note Taker/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Hotkeys;
+
+namespace Note_Taker
+{
+    public partial class Form1 : Form
+    {
+
+        private Hotkeys.GlobalHotkey ghk;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            ghk = new Hotkeys.GlobalHotkey(Constants.WIN , Keys.N, this);
+        }
+
+        private void HandleHotkey()
+        {
+            this.Visible = true;
+            this.Activate();
+            this.input_textbox.Focus();
+        }
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == Hotkeys.Constants.WM_HOTKEY_MSG_ID)
+                HandleHotkey();
+            base.WndProc(ref m);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             System.Diagnostics.Debug.WriteLine("Trying to register WIN+N");
+	            if (ghk.Register())
+                    System.Diagnostics.Debug.WriteLine("Hotkey registered.");
+	            else
+                    System.Diagnostics.Debug.WriteLine("Hotkey failed to register");            
+        }
+
+        private void Form1_FormClosing(object sender, EventArgs e)
+        {
+            if (!ghk.Unregiser())
+                MessageBox.Show("Hotkey failed to unregister!");
+        }
+
+        private void input_textbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13 ) // check if enter key hit
+            {
+                //Record input to file
+                LogNote(input_textbox.Text);
+                input_textbox.Text = "";
+                e.Handled = true;
+
+                //Hide form
+                this.Visible = false;
+            }
+        }
+
+        private void LogNote(string notetext)
+        {
+
+            notetext = FormatNote(notetext);
+
+            string filename = @"D:\Users\Richard\Documents\OutputFile.txt";
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(filename, true))
+            {
+                file.WriteLine(notetext);
+            }
+        }
+
+        private string FormatNote(string notetext)
+        {
+            string formatted_time = System.DateTime.Now.ToString("yyyy-MM-dd HH:mm");
+            string formatted_notetext = String.Format("{0}\t{1}", formatted_time, notetext);
+            return formatted_notetext;
+        }
+    }
+}
